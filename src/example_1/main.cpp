d75f272aae59ec4bca165207cc82630926c87d8e
Gustavo Sobral <ghsobral@gmail.com>
2016-08-14 19:24:01 -0300

    Add system trace to example 1

--- src/example_1/main.cpp
+++ src/example_1/main.cpp
@@ -16,6 +16,17 @@ int sc_main(int argc, char* argv[]) {
   monitor Monitor("Monitor");
   Monitor.A(ASig); Monitor.B(BSig); Monitor.F(FSig); Monitor.Clk(TestClk);
 
+  // System trace
+  sc_trace_file* Tf;
+  Tf = sc_create_vcd_trace_file("traces");
+  Tf->set_time_unit(10, SC_NS);
+  sc_trace(Tf, ASig, "A");
+  sc_trace(Tf, BSig, "B");
+  sc_trace(Tf, FSig, "F");
+  sc_trace(Tf, DUT.S1, "S1");
+  sc_trace(Tf, DUT.S2, "S2");
+  sc_trace(Tf, DUT.S3, "S3");
+
   sc_start();
   return 0;
 }
