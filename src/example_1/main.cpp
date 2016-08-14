762c441d52bcde967919db13c46697f9202b584a
Gustavo Sobral <ghsobral@gmail.com>
2016-08-14 17:48:34 -0300

    Refactor example 1 variables

--- src/example_1/main.cpp
+++ src/example_1/main.cpp
@@ -6,12 +6,15 @@
 int sc_main(int argc, char* argv[]) {
   sc_signal<bool> ASig, BSig, FSig;
   sc_clock TestClk("TestClock", 10, SC_NS, 0.5);
-  stimulus Stim1("Stimulus");
-  Stim1.A(ASig); Stim1.B(BSig); Stim1.Clk(TestClk);
+
+  stimulus Stim("Stimulus");
+  Stim.A(ASig); Stim.B(BSig); Stim.Clk(TestClk);
+
   exor2 DUT("exor2");
   DUT.A(ASig); DUT.B(BSig); DUT.F(FSig);
-  monitor Monitor1("Monitor");
-  Monitor1.A(ASig); Monitor1.B(BSig); Monitor1.F(FSig); Monitor1.Clk(TestClk);
+  
+  monitor Monitor("Monitor");
+  Monitor.A(ASig); Monitor.B(BSig); Monitor.F(FSig); Monitor.Clk(TestClk);
 
   sc_start();
   return 0;
