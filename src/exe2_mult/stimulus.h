418ca335f53d6df85c6d8a44ff2ed570c28435f8
Gustavo Sobral <ghsobral@gmail.com>
2016-08-19 10:13:27 -0300

    Add random numbers on stimulus

--- src/exe2_mult/stimulus.h
+++ src/exe2_mult/stimulus.h
@@ -1,18 +1,23 @@
 #include "systemc.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 SC_MODULE(stimulus) {
   sc_out<sc_uint<64> > A, B;
   sc_in<bool> Clk;
 
   void stim_gen() {
-    A.write(0); B.write(0); wait();
-    A.write(84); B.write(10); wait();
-    A.write(6); B.write(10); wait();
-    A.write(23); B.write(45); wait();
+    int i;
+    for(i = 0; i < 32; i++) {
+      A.write(rand() % 99 + 1); B.write(rand() % 99 + 1); wait();
+    }
     sc_stop();
   }
 
   SC_CTOR(stimulus) {
+    srand (time(NULL));
+
     SC_THREAD(stim_gen);
     sensitive << Clk.pos();
   }
